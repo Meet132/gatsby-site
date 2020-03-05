cb31fc6f2a5a26a385e994ca46019238b38c4bd7
Meet132 <mpmeet132@gmail.com>
2020-03-06 00:03:43 +0530

    gatsby site host

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,14 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    module.exports = {
+      pathPrefix: "/gatsby-site",
+    },
+    {
+      "scripts": {
+        "deploy": "gatsby build --prefix-paths && gh-pages -d public"
+      }
+    }
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
